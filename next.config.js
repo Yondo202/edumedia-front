1c6c91d37536ffd65bdb93bcb6c826f86add7ab8
infosystems-mn <contact@infosystems.mn>
2022-03-18 10:07:26 +0800

    config changed

--- next.config.js
+++ next.config.js
@@ -1,34 +1,32 @@
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
 
 module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
+  const isSTaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
 
-    const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-    const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
-    const isSTaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
-    
-    const env = {
-        serverUrl:(()=>{
-            if(isDev) return 'http://localhost:1380'
-            if(isProd) return 'https://4938-124-158-107-34.ngrok.io'
-            if(isSTaging) return 'https://4938-124-158-107-34.ngrok.io'
-        })(),
-        frontUrl:(()=>{
-            if(isDev) return 'http://localhost:3080'
-            if(isProd) return 'https://edumedia.vercel.app'
-            if(isSTaging) return 'https://edumedia.vercel.app'
-        })(),
-        categoryUrl:(()=>{
-            return 'p'
-        })(),
-        newsUrl:(()=>{
-            return 'news'
-        })(),
-       
-    }
-    return {
-        env
-    }
-}
+  const env = {
+    serverUrl: (() => {
+      if (isDev) return "http://localhost:1380";
+      if (isProd) return "https://edumedia.mn/api";
+      if (isSTaging) return "https://edumedia.mn/api";
+    })(),
+    frontUrl: (() => {
+      if (isDev) return "http://localhost:3080";
+      if (isProd) return "https://edumedia.mn";
+      if (isSTaging) return "https://edumedia.mn";
+    })(),
+    categoryUrl: (() => {
+      return "p";
+    })(),
+    newsUrl: (() => {
+      return "news";
+    })(),
+  };
+  return {
+    env,
+  };
+};
 
 // module.exports = withCSS(
 //   withImages({
@@ -78,10 +76,6 @@ module.exports = (phase) => {
 //     }
 //   })
 // )
-
-
-
-
 
 // module.exports = {
 //     images: {
